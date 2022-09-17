a71f8e020f3e38303fe338e56072bf0dc79d9d94
lisa LARADE <lisalarade@MacBook-Pro-de-lisa-2.local>
2022-09-17 13:47:02 +0200

    calculatrice2 version 2

--- calculatrice2.py
+++ calculatrice2.py
@@ -3,14 +3,7 @@ from curses.ascii import isdigit
 
 context = False
 
-a = input("Entrez un premier nombre: ")
-b = input("Entrez un deuxième nombre: ")
-
-"""c = input("Entrez un premier nombre: ")
-d = input("Entrez un deuxième nombre: ")"""
-
 def somme(x, y):
-    
     return int(x) + int(y)
 
 """if a.isdigit() and b.isdigit():
@@ -21,10 +14,10 @@ else:
 
 while context == False:
     try:
+        a = input("Entrez un premier nombre: ")
+        b = input("Entrez un deuxième nombre: ")
         if a.isdigit() and b.isdigit():
             context = True
             print(f"Le résultat de l'addition de {a} avec {b} est égal à  {somme(a, b)}")
     except ValueError:
         print("Veuillez entrer deux nombres valides")
-        if a.isdigit() and b.isdigit():
-            print(somme(a, b))
