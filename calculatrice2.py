8e495dc069671a3b66e940af64abeb2701a690a7
lisa LARADE <lisalarade@MacBook-Pro-de-lisa-2.local>
2022-09-17 13:53:29 +0200

    calculatrice2 version3: OK

--- calculatrice2.py
+++ calculatrice2.py
@@ -6,18 +6,13 @@ context = False
 def somme(x, y):
     return int(x) + int(y)
 
-"""if a.isdigit() and b.isdigit():
-    print(somme(a, b))
-else:
-    print("Veuillez entrer deux nombres valides")
-    print(somme(c, d))"""
 
 while context == False:
     try:
         a = input("Entrez un premier nombre: ")
         b = input("Entrez un deuxième nombre: ")
-        if a.isdigit() and b.isdigit():
-            context = True
-            print(f"Le résultat de l'addition de {a} avec {b} est égal à  {somme(a, b)}")
+        #if a.isdigit() and b.isdigit():
+        print(f"Le résultat de l'addition de {a} avec {b} est égal à  {somme(a, b)}")
+        context = True
     except ValueError:
         print("Veuillez entrer deux nombres valides")
