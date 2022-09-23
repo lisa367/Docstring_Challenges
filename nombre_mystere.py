6352a2c45dc27299b2149a5427484d372ba90e12
lisa367 <75017410+lisa367@users.noreply.github.com>
2022-09-23 16:52:40 +0200

    Challenge: Nombre mystere

--- nombre_mystere.py
+++ nombre_mystere.py
@@ -4,18 +4,20 @@ nombre_mystere = random.randint(0, 100)
 essais = 5
 win = False
 
+print("Nombre mystere: ", nombre_mystere)
 while essais > 0:
     choix = input("Devine le nombre: ")
     essais -= 1
-    if choix == nombre_mystere:
+    if int(choix) == nombre_mystere:
         win = True
         print(f"Bravo ! Le nombre mystère était bien {nombre_mystere} !")
-        print(f"Tu as trouvé le nombre en {essais} essais.")
+        print(f"Tu as trouvé le nombre en {5 - essais} essais.")
+        essais = 0
     else:
-        if choix > nombre_mystere:
-            print(f"Le nombre mystère est plus grand que {choix}")
-        else:
+        if int(choix) > nombre_mystere:
             print(f"Le nombre mystère est plus petit que {choix}")
+        else:
+            print(f"Le nombre mystère est plus grand que {choix}")
         print(f"Il te reste {essais} essais")
 
 if essais == 0 and win == False:
