eaac911d0f05dd847de7e5172c9df2e35641cf4a
lisa367 <75017410+lisa367@users.noreply.github.com>
2022-09-26 23:04:04 +0200

    Challenge: Trieur de fichiers

--- trieur_de_fichiers/trieur.py
+++ trieur_de_fichiers/trieur.py
@@ -2,5 +2,16 @@ from pathlib import Path
 
 working_directory = Path.cwd()
 files = [file for file in working_directory if file.is_file()]
+directories = [directory for directory in working_directory if directory.is_dir()]
 # print(working_directory)
 # print(files)
+# print(directories)
+
+for file in files:
+    extension = file.split(".")[-1]
+    if extension in directories:
+        pass
+    else:
+        new_dir = working_directory / extension
+        new_dir.mkdir(exist_ok=True)
+        # Déplacer le fichier dans le nouveau dossier
