6045bd8999c5aa48efedecaca39ef3530525b0d2
lisa367 <75017410+lisa367@users.noreply.github.com>
2022-10-01 23:14:59 +0200

    Challenge: Liste de course OOP

--- liste_de_course_OOP/lib.py
+++ liste_de_course_OOP/lib.py
@@ -12,6 +12,10 @@ class Liste:
         with open(self.file_name, 'w') as file:
             json.dump([], file)
 
+    def __post_init__(self):
+        with open(self.file_name, 'w') as file:
+            json.dump([], file)
+
 
     def __repr__(self):
         return f"Liste: {self.nom}"
