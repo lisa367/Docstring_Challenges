8978dab3bdcfab9203c3e543e6545baed0d7da82
lisa367 <75017410+lisa367@users.noreply.github.com>
2022-10-05 00:23:45 +0200

    Challenge: Jeu de role dans le terminal

--- jeu_de_role.py
+++ jeu_de_role.py
@@ -2,10 +2,13 @@ import random
 
 game_over = False
 
-vie_utilisateur = 50
+vie_joueur = 50
 vie_ennemi = 50
 
 potions_utilisateur = 3
 
-while vie_utilisateur > 0 or vie_ennemi > 0:
+while vie_joueur > 0 or vie_ennemi > 0:
     choix = input("Souhaitez-vous attaquer (1) ou utiliser une potion (2) ?")
+    if choix == 1:
+        attaque_joueur = random.randint(5, 10)
+        attaque_ennemi = random.randint(5, 15)
