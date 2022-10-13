12b5a769fd7a065866ff26993065b556cfe98761
lisa367 <75017410+lisa367@users.noreply.github.com>
2022-10-13 13:11:08 +0200

    added BlogPost model to admin interface

--- MyBlog/src/Posts/admin.py
+++ MyBlog/src/Posts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from Posts.models import BlogPost
 
 # Register your models here.
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ("title", "published", "created_on", "last_updated")
+    list_editable = ("published", )
+
+
+admin.site.register(BlogPost, BlogPostAdmin)
