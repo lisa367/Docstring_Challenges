4fbd13099ea424dfe63d5b5a645a0ff85de4d418
lisa367 <75017410+lisa367@users.noreply.github.com>
2022-10-13 12:18:55 +0200

    Posts app models

--- MyBlog/src/Posts/models.py
+++ MyBlog/src/Posts/models.py
@@ -1,3 +1,32 @@
+from tabnanny import verbose
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.template.defaultfilters import slugify
 
 # Create your models here.
+
+# class Author(models.Model):
+    # author = models.CharField(max_length=100)
+
+User = get_user_model()
+
+
+class BlogPost(models.Model):
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    title = models.CharField(max_length=255, unique=True, verbose_name="Titre")
+    slug = models.SlugField(max_length=255, unique=True, blank=True)
+    content = models.TextField(blank=True, verbose_name="Contenu")
+    created_on = models.DateField(blank=True, null=True)
+    last_updated = models.DateTimeField(auto_now=True)
+    published = models.BooleanField(default=False, verbose_name="Publié")
+
+    class Meta:
+        ordering = ['-created_on']
+        verbose_name = "Article"
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.slug = slugify(self.title)
