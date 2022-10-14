ac75ca6aba53addb86ce345b98326d1c22ade1b3
lisa367 <75017410+lisa367@users.noreply.github.com>
2022-10-14 20:40:15 +0200

    Posts app create blog post url

--- MyBlog/src/Posts/urls.py
+++ MyBlog/src/Posts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from Posts.views import BlogHomeView
+from Posts.views import BlogHomeView, BlogPostCreate
 
 app_name = "Posts"
 
 urlpatterns = [
     path('', BlogHomeView.as_view(), name="home"),
+    path('create/', BlogPostCreate.as_view(), name="create"),
 ]
