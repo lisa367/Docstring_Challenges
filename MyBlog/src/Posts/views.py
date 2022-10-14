23cdd41a30681163d026abfed92517e54f130342
lisa367 <75017410+lisa367@users.noreply.github.com>
2022-10-14 18:16:38 +0200

    Posts app homepage view

--- MyBlog/src/Posts/views.py
+++ MyBlog/src/Posts/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 
-from models import BlogPost
+from Posts.models import BlogPost
 # Create your views here.
 
 
 class BlogHomeView(ListView):
     model = BlogPost
-    template_name = ""
+    # template_name = ""
     context_object_name = "posts"
