33ee09b59b620a5f4cb2eab395854e32410f39bd
lisa367 <75017410+lisa367@users.noreply.github.com>
2022-10-15 15:26:06 +0200

    Posts app blog UpdateView url

--- MyBlog/src/Posts/urls.py
+++ MyBlog/src/Posts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from Posts.views import BlogHomeView, BlogPostCreate
+from Posts.views import BlogHomeView, BlogPostCreate, BlogPostUpdate
 
 app_name = "Posts"
 
 urlpatterns = [
     path('', BlogHomeView.as_view(), name="home"),
     path('create/', BlogPostCreate.as_view(), name="create"),
+    path('edit/<str:slug>/', BlogPostUpdate.as_view(), name="edit"),
 ]
