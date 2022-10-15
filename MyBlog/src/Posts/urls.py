89105d4a6ca2041d3535d95b3320ff9c27467999
lisa367 <75017410+lisa367@users.noreply.github.com>
2022-10-15 17:04:20 +0200

    Posts app restricted access urls

--- MyBlog/src/Posts/urls.py
+++ MyBlog/src/Posts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from Posts.views import BlogHomeView, BlogPostCreate, BlogPostUpdate, BlogPostDelete, BlogPostDetail
+# from django.contrib.auth.decorators import login_required
 
 app_name = "Posts"
 
@@ -9,4 +10,5 @@ urlpatterns = [
     path('edit/<str:slug>/', BlogPostUpdate.as_view(), name="edit"),
     path('delete/<str:slug>/', BlogPostDelete.as_view(), name="delete"),
     path('<str:slug>/', BlogPostDetail.as_view(), name="post"),
+    # path('create/', login_required(BlogPostCreate.as_view()), name="create"),
 ]
