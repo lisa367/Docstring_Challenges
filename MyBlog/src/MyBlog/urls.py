f5e3be5ca94b4f5f666fc4fe2e6f8f029a724120
lisa367 <75017410+lisa367@users.noreply.github.com>
2022-10-18 11:45:12 +0200

    added links to media files

--- MyBlog/src/MyBlog/urls.py
+++ MyBlog/src/MyBlog/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from src.MyBlog import settings
+from MyBlog import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
