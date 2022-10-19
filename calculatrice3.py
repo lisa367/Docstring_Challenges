2ce11f614219eb71e919f370b888623f408df6f1
lisa367 <75017410+lisa367@users.noreply.github.com>
2022-10-19 23:56:33 +0200

    Challenge: Calculatrice - operations multiples

--- calculatrice3.py
+++ calculatrice3.py
@@ -1,2 +1,8 @@
 # Calculatrice: Operations multiples
 
+
+operateurs = ["+", "-", "*", "/", "%"]
+
+#print("Veuillez entrer une opération : ")
+operation = input("Veuillez entrer une opération : ")
+operation_liste = operation.split("")
