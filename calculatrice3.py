f5af4c3d47615b9dd74170d9da627f78bb301095
lisa367 <75017410+lisa367@users.noreply.github.com>
2022-10-20 00:13:26 +0200

    Challenge: Calculatrice - operations multiples

--- calculatrice3.py
+++ calculatrice3.py
@@ -7,4 +7,16 @@ operateurs = ["+", "-", "*", "/", "%"]
 operation = input("Veuillez entrer une opération : ")
 operation_liste = operation.split("")
 
+if len(operation_liste) != 3:
+    print("Veuillez entrer une opération valide")
+
 operateur = operateurs.find(operation_liste[1])
+
+
+# Fonction de validation dess données:
+    # deux nombres et un operateur séparés d'un espace
+    # operation dans le bon ordre : regex
+
+# Re-demander un input tant que la fonction de validation ne retourne pas True
+
+# Effectuer l'opération et afficher le résultat dans le terminal
