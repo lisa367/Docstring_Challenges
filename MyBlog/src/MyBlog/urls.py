7bf4bef07b17fa99245904ee021db06f30d394bb
lisa367 <75017410+lisa367@users.noreply.github.com>
2022-10-25 02:21:23 +0200

    added landing page url

--- MyBlog/src/MyBlog/urls.py
+++ MyBlog/src/MyBlog/urls.py
@@ -22,6 +22,6 @@ from MyBlog.views import LandingPageView
 
 urlpatterns = [
     path('', LandingPageView.as_view(), name='landing-page'),
-    path('admin/', admin.site.urls, name='admin-site'),
+    path('admin/', admin.site.urls),
     path('blog/', include('Posts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
