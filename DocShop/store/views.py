1357ab2170c803ca38e917c98b3ace40ce8c8334
lisa367 <75017410+lisa367@users.noreply.github.com>
2022-11-01 17:44:04 +0100

    index view

--- DocShop/store/views.py
+++ DocShop/store/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from store.models import Product
 
 # Create your views here.
+
+
+def index(request):
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, "store/index.html", context=context)
+
+
+def detail(request, product_id):
+    product = Product.objects.get(pk=product_id)
+    return render(request, "store/detail.html", context={"product": product})
