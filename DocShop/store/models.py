324d86be28d379594572ce53e5eed3af3bdb2329
lisa367 <75017410+lisa367@users.noreply.github.com>
2022-11-01 14:58:30 +0100

    created the Product model

--- DocShop/store/models.py
+++ DocShop/store/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, blank=False)
+    price = models.FloatField(default=0.0)
+    stock = models.IntegerField(default=0)
+    description = models.TextField(blank=True)
+    thumbnail = models.ImageField(upload_to="products", blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Produit"
+        ordering = ["stock", "name"]
+
+    def __str__(self):
+        return f"{self.name} ({self.stock})"
