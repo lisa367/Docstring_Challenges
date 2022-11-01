7b5f861d9f28450baca0235395d722f4d9b91cdd
lisa367 <75017410+lisa367@users.noreply.github.com>
2022-11-01 15:50:58 +0100

    added the signup url

--- DocShop/shop/urls.py
+++ DocShop/shop/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from accounts.views import signup
 
 urlpatterns = [
+    path(''),
     path('admin/', admin.site.urls),
+    path('signup/', signup, name='signup'),
 ]
