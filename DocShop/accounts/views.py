4d419b6b6e080eaa35a0c5bf7e79694d0242802b
lisa367 <75017410+lisa367@users.noreply.github.com>
2022-11-01 16:09:33 +0100

    modified signup view

--- DocShop/accounts/views.py
+++ DocShop/accounts/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model, login, logout
+from django.shortcuts import render, redirect
 
-# Create your views here.
+User = get_user_model()
 
 
 def signup(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = User.objects.create_user(username=username, password=password)
+
+        login(request, user)
+        redirect('index')
+
     return render(request, "accounts/signup.html")
