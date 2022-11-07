12e7fcafb1e1a90cc1e150706b60e3282ce9b2e5
lisa367 <75017410+lisa367@users.noreply.github.com>
2022-11-07 15:04:24 +0100

    tests for bank.py Account class

--- TestsUnitaires/test_bank.py
+++ TestsUnitaires/test_bank.py
@@ -1,23 +1,32 @@
+import pytest
+
 from .bank import Account
 
 
-def test_deposit():
-    account = Account(initial_balance=1000)
+@pytest.fixture
+def account():
+    return Account(initial_balance=1000)
+
+
+def test_deposit(account):
     account.deposit(1000)
     assert account.balance == 2000
 
 
-def test_withdraw():
-    account = Account(initial_balance=1000)
+def test_withdraw(account):
     account.withdraw(500)
     assert account.balance == 500
 
 
-def test__create_identifier_length_of_identifier():
-    account = Account(initial_balance=1000)
+def test_withdraw_not_enough_money():
+    account = Account(initial_balance=200)
+    with pytest.raises(ValueError):
+        account.withdraw(amount=500)
+
+
+def test__create_identifier_length_of_identifier(account):
     assert len(account.identifier) == 25
 
 
-def test__create_identifier_is_alnum():
-    account = Account(initial_balance=1000)
+def test__create_identifier_is_alnum(account):
     assert account.identifier.isalnum() is True
