c196a40a4d3052d28ae2e90a5fb24558870452a5
lisa367 <75017410+lisa367@users.noreply.github.com>
2022-11-10 11:07:14 +0100

    supprimer un article du panier

--- DocShop/store/forms.py
+++ DocShop/store/forms.py
@@ -5,7 +5,16 @@ from store.models import Order
 
 class OrderForm(forms.ModelForm):
     quantity = forms.ChoiceField(choices=[(i, i) for i in range(1, 11)])
+    delete = forms.BooleanField(default=False, required=False, label="Supprimer")
 
     class Meta:
         model = Order
         fields = ["quantity"]
+
+    def save(self, *args, **kwargs):
+        if self.cleaned_data["delete"] :
+            # return self.instance.delete()
+            if self.instance.user.cart.orders.count() == 0:
+                self.instance.user.cart.delete()
+            return True
+        return super().save(*args, **kwargs)
